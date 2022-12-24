c0897e84d085f2bde7bde325286ff1df5990a967
Davi Sonda Sonego <davisonego16@gmail.com>
2022-12-23 21:31:14 -0300

    Criando APi

---
+++ api-mwl/start/routes.ts
@@ -0,0 +1,4 @@
+/* eslint-disable prettier/prettier */
+import './routes/health';
+import './routes/teste';
+
---
+++ api-mwl/start/routes/health.ts
@@ -0,0 +1,7 @@
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.get('health', async ({ response }) => {
+  const report = await HealthCheck.getReport()
+  return report.healthy ? response.ok(report) : response.badRequest(report)
+}).prefix('mwl')
